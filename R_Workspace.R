096e43d854450b53f049090c7c5f04c1fcf5b22c
Ashish Manoj J <122615401+Ash-Manoj@users.noreply.github.com>
2024-10-23 14:19:13 +0200

    changes

---
+++ R_Workspace.R
@@ -0,0 +1,56 @@
+#library('Catflow')
+# OR Initialise own version of R package codes
+# initialize Catflow
+library('Catflow')
+library(zoo)
+
+testde_3 <- read.csv("C:/Users/as2023/bwSyncShare/01_Analysis/02_Europe_Flood/03_CATFLOW/in/climate/testde_3.csv")
+
+data_era5 = zoo(testde_3$total_precipitation_sum, order.by = as.Date(testde_3$date))
+data_lstm = zoo(testde_3$testde_2, order.by = as.Date(testde_3$date))
+data_eobs = zoo(testde_3$eobs, order.by = as.Date(testde_3$date))
+data_dwd = zoo(testde_3$Daily_Precipitation, order.by = as.Date(testde_3$date))
+
+write.precip(data_era5,'Precip_era5.dat',start.time='02.01.1980 00:00:00.00'
+             ,time.unit='d',faktor.p = 1)
+
+# Reading Output Files
+setwd("C:/Users/ashish/bwSyncShare/01_Analysis/01_Krebsbach_Reservoir/W22/CATFLOW")
+system( './Catflow.exe' , show.output.on.console = TRUE, invisible = FALSE, wait=T)
+write.printout(output.file = "printout.prt",
+               start.time = "08.06.2016 00:00:00",
+               end.time = "09.06.2016 00:00:00",
+               intervall = 300, time.unit = "s",
+               flag = 1)
+
+geometry = read.geofile('./in/hillgeo/rep_hill.geo')
+soilmoisture = read.catf.resultmat('./out/theta.out')
+soil = read.soil.mat(input.file = "boden.dat")
+colfunc <- colorRampPalette(c("orange", "blue"))
+colfunc(10)
+colorsForCuts = colfunc(5)
+lowercut =0.20
+uppercut =0.50
+lencut =4
+plot.catf.movie(soilmoisture,geometry,begindate='08.06.2016 00:00:00.00')
+resultmat = soilmoisture
+geof = geometry
+color.codes()
+
+plot.catf.grid(x = geometry$sko, y = geometry$hko, 
+                sel = NULL, boundcol = 1, 
+               plotting = TRUE, plotpoints = F)
+
+# Reading Output Files
+
+
+# Extracting from Raster
+setwd("C:/Users/ashish/bwSyncShare/01_Analysis/01_Krebsbach_Reservoir/W22/ClimateData")
+SM = "SoilMoisture.grib"
+library(terra)
+##Downloading file
+grib_data <- terra::rast(SM)
+print(grib_data)
+
+## Convert to data frame
+df <- as.data.frame(grib_data, xy=TRUE)
